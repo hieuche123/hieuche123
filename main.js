3796a893669780df4fb961b3709d951ea2d11e36
hieuche123 <86963416+hieuche123@users.noreply.github.com>
2021-07-06 21:08:59 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const huhu = $('html')
+huhu.onclick = function () {
+    audio.play();
+}
+console.log(audio)
