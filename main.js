0f591793e85a4cb063c28793439f2cb0c0a97ffc
hieuche123 <86963416+hieuche123@users.noreply.github.com>
2021-07-07 10:50:18 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 const huhu = $('html')
+const hihi = $('.music')
+
+console.log(huhu)
+console.log(hihi)
 huhu.onclick = function () {
-    audio.play();
+    hihi.play();
 }
-console.log(audio)
